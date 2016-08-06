5804df96fc0f1a4de5bba3e1364abc7461c2305f
Termina1 <terminal2010@gmail.com>
2016-08-06 17:52:05 +0300

    createbucket if not exist, instead of create bucket

--- store.go
+++ store.go
@@ -11,8 +11,10 @@ func storeAndLink(db *bolt.DB, secret string) (string, error) {
 
   uid := uuid.NewV4().String()
   err := db.Update(func(tx *bolt.Tx) error {
-    tx.CreateBucket([]byte(bucket))
-    b := tx.Bucket([]byte(bucket))
+    b, berr := tx.CreateBucketIfNotExists([]byte(bucket))
+    if berr != nil {
+      return berr
+    }
     err := b.Put([]byte(uid), []byte(secret))
     return err
   })
@@ -23,8 +25,10 @@ func readAndDelete(db *bolt.DB, uid string) (string, error) {
   var result []byte;
   var copyDest []byte;
   err := db.Batch(func(tx *bolt.Tx) error {
-    tx.CreateBucket([]byte(bucket))
-    b := tx.Bucket([]byte(bucket))
+    b, berr := tx.CreateBucketIfNotExists([]byte(bucket))
+    if berr != nil {
+      return berr
+    }
     result = b.Get([]byte(uid))
     err := b.Delete([]byte(uid))
     // bolt will reuse memory after transation, we need to topy it
